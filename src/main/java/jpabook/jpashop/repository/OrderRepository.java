4d62922218e47e9ce55b95fe020385be51b596e6
mincheol <kmc04080408@gmail.com>
2021-07-19 15:33:43 +0900

    ADD : 주문 도메인 구현

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,5 +19,11 @@ public class OrderRepository {
 
     public Order findOne(Long id) {
         return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        List<Order> resultList = em.createQuery("select o from Order o join o.member m", Order.class)
+                .setMaxResults(1000) //최대 1000
+                .getResultList();
     }
 }
