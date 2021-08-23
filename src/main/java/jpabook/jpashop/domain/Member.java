8707e8fca2ad62b3403ccf11d0c64030be733aab
mincheol <kmc04080408@gmail.com>
2021-08-23 20:26:13 +0900

    ADD : 엔티티 직접 노출 로직과 그에 따른 설정들

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +23,7 @@ public class Member {
     @Embedded  //내장 타입
     private Address address;
 
+    @JsonIgnore // 양방향 연관관계에 있으면 한쪽은 JsonIgnore 해줘야 함.
     @OneToMany(mappedBy = "member") //  order 테이블에 있는 member 필드에 의해 매핑됐다는 의미
     private List<Order> orders = new ArrayList<>();
 
